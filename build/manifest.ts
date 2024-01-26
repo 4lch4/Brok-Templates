0d4538d98f9c7e9ef5d7a38b176749c02883703c
4lch4 <git@4lch4.email>
2024-01-26 00:36:59 -0600

    feat: added beginning of automated manifest updates

---
+++ build/manifest.ts
@@ -0,0 +1,56 @@
+import fs from 'fs-extra'
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
+import pc from 'picocolors'
+import { glob } from 'glob'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+type Manifest = {
+  id: string
+  name: string
+  description: string
+}
+
+export async function main() {
+  try {
+    // const manifests = await fs.readdir(join(__dirname, '..'), { recursive: true })
+    const manifestFilePaths = await glob('**/template.json', {
+      cwd: join(__dirname, '..'),
+      absolute: true,
+      maxDepth: 2,
+    })
+
+    const manifests: Manifest[] = []
+
+    for (const filePath of manifestFilePaths) {
+      const manifest = await Bun.file(filePath).json()
+      manifests.push(manifest)
+    }
+
+    console.debug(pc.cyan('[SUCCESS][main]: Found the following manifests:'))
+    console.debug(pc.cyan(JSON.stringify(manifests, null, 2)))
+
+    await fs.writeJSON(join(__dirname, '..', 'manifest.json'), manifests, {
+      spaces: 2,
+    })
+
+    console.log(pc.green('[SUCCESS][main]: Wrote manifest to file.'))
+  } catch (error) {
+    console.error(`[ERROR][main]: An error has been caught:`)
+    console.error(error)
+  }
+}
+
+main()
+  .then(() => {
+    console.log(pc.green('[SUCCESS][main]: Brok has finished running.'))
+
+    process.exit(0)
+  })
+  .catch(err => {
+    console.error(pc.red('[ERROR][main]: Brok has encountered an uncaught error.'))
+    console.error(err)
+
+    process.exit(1)
+  })
